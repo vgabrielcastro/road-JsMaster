f1d87fc0a687d9c3dc226485bb55eab85b2e0c5f
Vinicius Gabriel <vini.gabriel@outlook.com>
2023-12-22 07:15:51 -0300

    finish scrip handle

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 // get filter element
+const filterElement = document.querySelector('header input')
+
 // get cards elements
+const cards = document.querySelectorAll('.cards li')
 
 // add input event for the filter element
+filterElement.addEventListener('input', filterCards)
 
 // filter function
-// if the filter is not empty
-// for each card of cards
-// get card heading (title)
-// tranform to lower case
-// transform filter text to lower case
-// if card title does not include the filter text
-// hide the card element
-// else
-// unhide the card element
-// else
-// for each card of cards
-// unhide the card element
+function filterCards() {
+    // if the filter is not empty
+    if (filterElement.value !== '') {
+        // for each card of cards
+        for (let card of cards) {
+            // get card heading (title)
+            let titleElement = card.querySelector('h2');
+            // get card title text content
+            let title = titleElement.textContent.toLowerCase();
+            // transform filter text to lower case
+            let filterText = filterElement.value.toLowerCase();
+            // if card title does not include the filter text
+            if (!title.includes(filterText)) {
+                // hide the card element
+                card.style.display = 'none';
+            } else {
+                // unhide the card element
+                card.style.display = 'block';
+            }
+        }
+    } else {
+        // for each card of cards
+        for (let card of cards) {
+            // unhide the card element
+            card.style.display = 'block';
+        }
+    }
+}
