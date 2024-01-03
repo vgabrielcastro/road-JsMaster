0e6c15fd17e4f7be255288ec4e9821f8de7e84e3
Vinicius Gabriel <vini.gabriell@outlook.com>
2024-01-03 19:22:02 -0300

    update function add forEach and new feature js

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 ## Projeto para estudo e melhorar em js!!
+
+adicionar a função de pesquisa os itens da lista dos cards:
+
+- get filter element
+- get cards elements
+- add input event for the filter element
+- filter function
+- if the filter is not empty
+- for each card of cards
+- get card heading (title)
+- get card title text content
+- transform filter text to lower case
+- if card title does not include the filter text
+- hide the card element
+- un hide the card element
+- for each card of cards
+- unhide the card element
--- script.js
+++ script.js
@@ -1,38 +1,23 @@
-// get filter element
-const filterElement = document.querySelector('header input')
+const filterElement = document.querySelector('header input');
+const cards = document.querySelectorAll('.cards li');
 
-// get cards elements
-const cards = document.querySelectorAll('.cards li')
+filterElement.addEventListener('input', filterCards);
 
-// add input event for the filter element
-filterElement.addEventListener('input', filterCards)
-
-// filter function
 function filterCards() {
-    // if the filter is not empty
-    if (filterElement.value !== '') {
-        // for each card of cards
-        for (let card of cards) {
-            // get card heading (title)
-            let titleElement = card.querySelector('h2');
-            // get card title text content
-            let title = titleElement.textContent.toLowerCase();
-            // transform filter text to lower case
-            let filterText = filterElement.value.toLowerCase();
-            // if card title does not include the filter text
-            if (!title.includes(filterText)) {
-                // hide the card element
-                card.style.display = 'none';
-            } else {
-                // unhide the card element
-                card.style.display = 'block';
-            }
-        }
+    const filterText = filterElement.value.toLowerCase();
+
+    cards.forEach(card => {
+        const titleElement = card.querySelector('h2');
+        const title = titleElement.textContent.toLowerCase();
+        const shouldDisplay = title.includes(filterText);
+        toggleCardDisplay(card, shouldDisplay);
+    });
+}
+
+function toggleCardDisplay(card, shouldDisplay) {
+    if (shouldDisplay) {
+        card.classList.remove('hidden');
     } else {
-        // for each card of cards
-        for (let card of cards) {
-            // unhide the card element
-            card.style.display = 'block';
-        }
+        card.classList.add('hidden');
     }
 }
